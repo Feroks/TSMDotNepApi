3a86b1ba0c167b80979955c4bce7855a1168199b
Feroks <mp@best.lv>
2017-02-11 18:43:34 +0200

    Added Test conditions.

--- TSMDotNetApi.Tests/ItemRealmDataTests.cs
+++ TSMDotNetApi.Tests/ItemRealmDataTests.cs
@@ -22,6 +22,14 @@ namespace TSMDotNetApi.Tests
         public async Task GetItemRealmData_31336()
         {
             var data = await _tsmExplorer.GetItemRealmDataAsync(TsmRegion.Eu, RealmName, 31336);
+
+            Assert.AreEqual("Blade of Wizardry", data.Name);
+            Assert.AreEqual("Weapon", data.Class);
+            Assert.AreEqual("Sword", data.SubClass);
+            Assert.AreEqual(31336, data.Id);
+            Assert.AreEqual(0, data.VendorBuy);
+            Assert.AreEqual(432949, data.VendorSell);
+            Assert.AreEqual(100, data.Level);
         }
     }
 }
